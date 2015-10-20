70bbdcd724bdbc949e52b916ee2eb91e0b53cc97
scalakid <alaniit007@gmail.com>
2015-10-20 18:42:44 +1100

    Saving scala while loopprogram to GitHub

---
+++ src/ScalaWhileLoop.scala
@@ -0,0 +1,16 @@
+/**
+ * Created by ADMIN on 20-Oct-15.
+ * Simple scala while loop to print numbers from 1 to 10
+ */
+object ScalaWhileLoop {
+
+  def main(args:Array[String]): Unit =
+  {
+    var i=1
+    while(i<=10)
+      {
+        println(i)
+        i+=1
+      }
+  }
+}
